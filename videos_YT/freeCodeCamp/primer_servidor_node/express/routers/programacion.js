5180702dc2ffb9e07bf5df7f0ed05b6b15fb2774
Tomas Machin <tm.esteve@gmail.com>
2025-09-19 11:40:22 +0200

    mas metodos HTTP

--- videos_YT/freeCodeCamp/primer_servidor_node/express/routers/programacion.js
+++ videos_YT/freeCodeCamp/primer_servidor_node/express/routers/programacion.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { programacion } = require('../datos/cursos').infoCursos;
 const routerProgramacion = express.Router();
 
+// MIDDLEWARE
+routerProgramacion.use(express.json());     // para procesar el cuerpo de la solicitud en formato JSON en nuestro codigo
+
 routerProgramacion.get('/', (req, res) => {     // porque la ruta ya esta definida como /api/cursos/programacion y solo se añadiria lo siguiente
     res.send(JSON.stringify(programacion));
 });
@@ -13,7 +16,7 @@ routerProgramacion.get('/:lenguaje', (req, res) => {   // parametro URL -> :leng
     const lenguaje = req.params.lenguaje;   // lenguaje en req.params.lenguaje es el que hemos puesto como parametro URL
     const resultado = programacion.filter(curso => curso.lenguaje === lenguaje);
     // puede estar vacio asi q lo comprobamos
-    if (resultado.length === 0) return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+    if (resultado.length === 0) return res.status(204).send(`No se encontraron cursos de ${lenguaje}`);
 
     // PARAMETRO QUERY -> ordenar segun el numero de vistas de cada lenguaje
     if (req.query.ordenar === 'vistas') {
@@ -27,9 +30,47 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
     const lenguaje = req.params.lenguaje;
     const nivel = req.params.nivel;
     const resultado = programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
-    if (resultado.length === 0) return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+    if (resultado.length === 0) return res.status(204).send(`No se encontraron cursos de ${lenguaje}`);
+        // return res.status(404).end();   // para no enviar cuerpo
+    // res.send(resultado);    // no es necesario hacer: JSON.stringify(resultado) pq se hace automaticamente con JS
+    res.json(resultado);       // para asegurarse que envias la respuesta en formato JSON (los convierte)
+    // res.send() acepta diferentes valores para enviar como respuesta
+});
 
-    res.send(JSON.stringify(resultado));
+routerProgramacion.post('/', (req, res) => {
+    const cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));     // sin el middleware añadiria 'null'
+});
+
+routerProgramacion.put('/:id', (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);     // no es === pq estaria comparando int con string y el comparador estricto siempre devolveria falso
+    // comprobar que la salida ha sido exitosa (-1 = error)
+    if (indice >= 0) programacion[indice] = cursoActualizado;   // cambia el objeto entero
+    else res.status(204);   // añadir codigos de estado a las funciones - SABER CUAL ES 
+
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.patch('/:id', (req, res) => {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);
+    if (indice >= 0) {
+        const cursoAModificar = programacion[indice];
+        Object.assign(cursoAModificar, infoActualizada);    // cambiar solo unas propiedas en concreto no el objeto entero
+    }
+
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);
+    if (indice >= 0) programacion.splice(indice, 1);    // aliminar desde el indice un solo elemento
+    res.send(JSON.stringify(programacion));
 });
 
 module.exports = routerProgramacion;
